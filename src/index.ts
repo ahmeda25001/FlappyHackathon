5ff89fc2fbb663a20f7cc71050b8e11d9416dabb
ahmeda25001 <77513800+ahmeda25001@users.noreply.github.com>
2021-02-21 01:31:25 -0500

    Added pipes and pipe movements

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,23 @@ let bird = new Sprite(Texture.from("/assets/sprites/yellowbird_01.png"));
 
 let pipe = new Sprite(Texture.from("/assets/sprites/pipe-green.png"));
 let pipe2 = new Sprite(Texture.from("/assets/sprites/pipe-green.png"));
+let pipe3 = new Sprite(Texture.from("/assets/sprites/pipe-green.png"));
+let pipe4 = new Sprite(Texture.from("/assets/sprites/pipe-green.png"));
 pipe2.scale.x = -1;
 pipe.x = WIDTH;
 pipe.y = HEIGHT - 100;
 pipe2.x = WIDTH;
 pipe.y = 100
 
+const resetPipe1 = () => {
+	pipe.x = WIDTH;
+	pipe2.x = WIDTH;
+}
+
+const resetPipe2 = () => {
+	pipe3.x = WIDTH;
+	pipe4.x = WIDTH;
+}
 const gravity = () => {
 	bird.y += 4;
 	pipe.x -= 2;
@@ -33,6 +44,12 @@ const runningLoop = () => {
 	gravity();
 	if (bird.x === pipe.x) {
 		score++;
+	}
+	if (pipe.x < 0) {
+		resetPipe1
+	}
+	if (pipe3.x < 0) {
+		resetPipe2
 	}
   // Gravity - Push the Bird sprite downwards
   // Move the pipes to the left, move pipe on the left once it goes out of the viewport to the right of the screen and keep cycling pipes
